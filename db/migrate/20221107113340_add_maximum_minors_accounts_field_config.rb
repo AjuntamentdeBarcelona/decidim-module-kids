eb0e936ce6e1f845203c997b19a936a3a0b0a91d
antopalidi <t.opalidisanna@gmail.com>
2022-11-07 20:40:11 +0100

    fix migration name

--- db/migrate/20221107113340_add_maximum_minors_accounts_field_config.rb
+++ db/migrate/20221107113340_add_maximum_minors_accounts_field_config.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddMaximumMinorsAccountsField < ActiveRecord::Migration[6.1]
+class AddMaximumMinorsAccountsFieldConfig < ActiveRecord::Migration[6.1]
   def change
     change_table :decidim_kids_organization_configs do |t|
       t.integer :maximum_minors_accounts, null: false, default: 3
