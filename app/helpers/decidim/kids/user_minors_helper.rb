595cc3b38c11e6ed92a39b8399be4bef1e4ffe06
antopalidi <t.opalidisanna@gmail.com>
2022-11-07 20:33:51 +0100

    refactoring: add user_minors_helper for button 'Add minor account'

---
+++ app/helpers/decidim/kids/user_minors_helper.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Kids
+    module UserMinorsHelper
+      def button_to_add_minor_account
+        link_to(
+          t(".add"),
+          decidim_kids.new_user_minor_path,
+          class: "button small #{button_klass}"
+        )
+      end
+
+      def can_add_minor_account?
+        return if current_user.minors.count > Decidim::Kids.maximum_minors_accounts
+
+        true
+      end
+
+      private
+
+      def button_klass
+        return if can_add_minor_account?
+
+        "disabled"
+      end
+    end
+  end
+end
