9449aecf29633f8fc64c84b4bd4773525208cd24
antopalidi <t.opalidisanna@gmail.com>
2022-11-11 12:20:17 +0100

    add test create_minor_account (draft)

--- spec/commands/create_minor_account_spec.rb
+++ spec/commands/create_minor_account_spec.rb
@@ -5,24 +5,59 @@ require "spec_helper"
 module Decidim::Kids
   describe CreateMinorAccount do
     describe "call" do
-      let(:form) do
-        Decidim::Kids::MinorAccountForm.new(params)
+      let!(:organization) { create(:organization) }
+      let!(:tutor) { create(:user, :confirmed, organization:) }
+      let!(:minor) { Decidim::User.new(user_params) }
+      let!(:minor_data) { MinorData.new(minor_data_params) }
+      let!(:minor_account) do
+        {
+          tutor:,
+          minor:
+        }
       end
 
-      let(:command) { described_class.new(form, current_user) }
-      let(:params) do
+      let!(:form) do
+        MinorAccountForm.new(form_params)
+      end
+
+      let!(:command) { described_class.new(form, tutor) }
+
+      let(:form_params) do
         {
           name: "Mike",
           email: "test@example.com",
           birthday: "01/11/2009",
           password: "testpassword123456",
-          confirmation_password: "testpassword123456",
-          tos_agreement: true
+          password_confirmation: "testpassword123456",
+          tos_agreement: true,
         }
+      end
 
-        it "returns a valid response" do
-          expect { command.call }.to broadcast(:ok)
-        end
+      let(:user_params) do
+        {
+          email: "test@example.com",
+          name: "Mike",
+          organization: organization,
+          password: "testpassword123456",
+          password_confirmation: "testpassword123456",
+          nickname: "Mike",
+          tos_agreement: true,
+          blocked: true,
+          minor_data:
+        }
+      end
+
+      let(:minor_data_params) do
+        {
+          name: "Mike",
+          email: "test@example.com",
+          birthday: "01/11/2009",
+          password: "testpassword123456"
+        }
+      end
+
+      it "returns a valid response" do
+        expect { command.call }.to broadcast(:ok)
       end
     end
   end
