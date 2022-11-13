1ececd489aae9286dbe52d66fdf7108499645cfb
antopalidi <t.opalidisanna@gmail.com>
2022-11-13 11:56:30 +0100

    add tests: create minor's account

--- spec/commands/create_minor_account_spec.rb
+++ spec/commands/create_minor_account_spec.rb
@@ -8,12 +8,12 @@ module Decidim::Kids
       let!(:current_user) { create(:user, :confirmed) }
 
       let!(:form) do
-        Decidim::Kids::MinorAccountForm.new(form_params)
+        Decidim::Kids::MinorAccountForm.from_params(form_params)
       end
 
       let!(:command) { described_class.new(form, current_user) }
 
-      let(:form_params) do
+      let!(:form_params) do
         {
           name: "Mike",
           email: "test@example.com",
@@ -24,8 +24,40 @@ module Decidim::Kids
         }
       end
 
-      it "broadcasts :ok and creates the minor" do
-        expect { command.call }.to broadcast(:ok)
+      describe "when the form is valid" do
+        it "broadcasts :ok and creates the minor" do
+          expect { command.call }.to broadcast(:ok)
+        end
+
+        it "creates a new user" do
+          expect { command.call }.to change(Decidim::User, :count).by(1)
+        end
+
+        it "creates a new minor's account" do
+          expect { command.call }.to change(MinorAccount, :count).by(1)
+        end
+      end
+
+      describe "when the form is not valid" do
+        before do
+          allow(form).to receive(:invalid?).and_return(true)
+        end
+
+        it "broadcasts invalid" do
+          expect { command.call }.to broadcast(:invalid)
+        end
+
+        it "doesn't create a user" do
+          expect do
+            command.call
+          end.not_to change(Decidim::User, :count)
+        end
+
+        it "doesn't create a minor's account" do
+          expect do
+            command.call
+          end.not_to change(Decidim::Kids::MinorAccount, :count)
+        end
       end
     end
   end
