36cc4615295addea7a22a6d30d10bb26480f0b9c
Anna Topalidi <t.opalidisanna@gmail.com>
2022-11-29 18:52:31 +0100

    add deface close session btn

--- app/controllers/concerns/decidim/kids/impersonate_minors.rb
+++ app/controllers/concerns/decidim/kids/impersonate_minors.rb
@@ -6,8 +6,13 @@ module Decidim
       extend ActiveSupport::Concern
 
       included do
+        helper_method :impersonation_log_minor
         def current_user
           @current_user ||= managed_minor || managed_user || real_user
+        end
+
+        def impersonation_session_ends_at
+          @impersonation_session_ends_at ||= minor_session_ends_at || user_session_ends_at
         end
 
         private
@@ -25,6 +30,22 @@ module Decidim
 
         def expired_log_minor
           @expired_log ||= Decidim::Kids::ImpersonationMinorLog.where(tutor: real_user).expired.first
+        end
+
+        def current_user_impersonated?
+          current_user && (impersonation_log_minor.present? || impersonation_log.present?)
+        end
+
+        def minor_session_ends_at
+          return if impersonation_log_minor.blank?
+
+          impersonation_log_minor.started_at + Decidim::Kids::ImpersonationMinorLog::SESSION_TIME_IN_MINUTES.minutes
+        end
+
+        def user_session_ends_at
+          return if impersonation_log.blank?
+
+          impersonation_log.started_at + Decidim::ImpersonationLog::SESSION_TIME_IN_MINUTES.minutes
         end
       end
     end
