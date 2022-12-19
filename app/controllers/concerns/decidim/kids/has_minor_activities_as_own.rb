06de07de7dc92ef9b3762fbf7c881f062165b0ca
Alejandro Puerta <alejandro.puerta@nazaries.com>
2022-12-19 17:15:15 +0100

    init: concern to show minor activity in tutor account with private resources type, added

---
+++ app/controllers/concerns/decidim/kids/has_minor_activities_as_own.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Kids
+    module HasMinorActivitiesAsOwn
+      extend ActiveSupport::Concern
+
+      included do
+        alias_method :user_own_activities?, :own_activities?
+        def own_activities?
+          user_own_activities? || current_user.minors.include?(user)
+        end
+      end
+    end
+  end
+end
