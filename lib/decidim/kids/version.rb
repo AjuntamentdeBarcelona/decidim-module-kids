dcdb5f293312e4741a51ace579a301022e6b618d
Francisco Bolívar <francisco.bolivar@nazaries.com>
2024-02-14 13:52:55 +0100

    Upgrade decidim version

--- lib/decidim/kids/version.rb
+++ lib/decidim/kids/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-meetings version.
   module Kids
     VERSION = "0.1.0"
-    DECIDIM_VERSION = "0.27.2"
+    DECIDIM_VERSION = "0.27.5"
     COMPAT_DECIDIM_VERSION = [">= 0.27", "< 0.28"].freeze
   end
 end
