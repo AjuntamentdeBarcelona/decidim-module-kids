1e2fa362a2ebbab6572e93b519a8f465376445da
Alejandro Rueda <alejandro.rueda@nazaries.com>
2024-10-28 09:07:05 +0100

    Module version updated

--- lib/decidim/kids/version.rb
+++ lib/decidim/kids/version.rb
@@ -3,7 +3,7 @@
 module Decidim
   # This holds the decidim-meetings version.
   module Kids
-    VERSION = "0.2.0"
+    VERSION = "0.3.0"
     DECIDIM_VERSION = { git: "https://github.com/decidim/decidim", branch: "release/0.29-stable" }.freeze
     COMPAT_DECIDIM_VERSION = [">= 0.29", "< 0.30"].freeze
   end
